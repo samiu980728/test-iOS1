032413d32e36ea7767d3539dd2116d7e2cbdebad
萨缪 <samiu@samiudeMacBook-Pro.local>
2019-01-25 19:58:16 +0800

    test

---
+++ testIOS/testIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testIOS
+//
+//  Created by 萨缪 on 2019/1/25.
+//  Copyright © 2019年 萨缪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testIOS/testIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testIOS
+//
+//  Created by 萨缪 on 2019/1/25.
+//  Copyright © 2019年 萨缪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
